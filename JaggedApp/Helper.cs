4692893e237877d89c3f51be92018b1d527b955f
EROL AKGÜL <erolakgul88@gmail.com>
2021-10-06 13:45:01 +0300

    Add project files.

---
+++ JaggedApp/Helper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace JaggedApp
+{
+    public class Helper
+    {
+        //// koleksiyonu doldur
+        public static List<int> iList(List<int> _iList, int num)
+        {
+            for (int i = 1; i <= num; i++) _iList.Add(i);
+            return _iList;
+        }
+
+        //  array inner size ını belirle
+        public static int[][] declare(int[][] arr,int outerSize,int innerSize)
+        {
+            for (int i = 0; i < outerSize; i++) arr[i] = new int[innerSize];
+            return arr;
+        }
+
+        //// array i doldur
+        public static void setJagged(int[][] array, int i, int j, int value)
+        {
+            array[i][j] = value;
+        }
+
+        // değerleri göster
+        public static void display(int[][] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                System.Console.Write("Array({0}): ", i);
+
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    System.Console.Write("{0}{1}", arr[i][j], j == (arr[i].Length - 1) ? "" : " ");
+                }
+                System.Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ JaggedApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace JaggedApp
+{
+    class Program
+    {
+        /// <summary>
+        /// 1.adım parametrelerin tanımlanması
+        /// 2.adım koleksiyonun doldurulması
+        /// 3.adım array in iç size ının belirlenmesi
+        /// 4.adım jagged array in koleksiyondaki veri ile doldurulması
+        /// 5.adım jagged array deki verinin gösterilmesi
+        /// </summary>
+        
+        static void Main(string[] args)
+        {
+            #region init 20x10 matris 
+            int jsize = 0;
+            int size = 0;
+            const int jaggedInnerSize = 10;
+            const int jaggedOuterSize = 20; 
+            int[][] arr = new int[jaggedOuterSize][];  // outer size belirlenir // int[][] arr = new int[20][]; 
+            List<int> _iList = new List<int>();
+            #endregion
+
+            #region koleksiyonu doldur
+            _iList = Helper.iList(_iList, 200);
+            #endregion
+
+            #region array in iç size ını belirle
+            arr = Helper.declare(arr,jaggedOuterSize,jaggedInnerSize);
+            #endregion
+
+            #region doldur jagged array
+            foreach (var item in _iList)
+            {
+                if (size == jaggedInnerSize)
+                {
+                    size = 0;
+                    jsize++;
+
+                    if (jsize == jaggedOuterSize) break;
+
+                    Helper.setJagged(arr, jsize, size, item);
+                }
+                else
+                {
+                    Helper.setJagged(arr, jsize, size, item);
+                }
+                size++;
+            }
+            #endregion
+
+            #region göster
+            Helper.display(arr);
+            #endregion
+
+
+            System.Console.ReadKey();
+        }
+
+    }
+}
